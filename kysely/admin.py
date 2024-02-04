0c8bdaf2962aec7b9ec7cfcdba50fa42204a944b
wimpula <156064473+wimpula@users.noreply.github.com>
2024-02-04 14:49:48 +0200

    ...

--- kysely/admin.py
+++ kysely/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib import admin
-
-from .models import Kysymys
+from .models import Kysymys, Vaihtoehto
 
 admin.site.register(Kysymys)
+
+@admin.register(Vaihtoehto)
+class VaihtoehtoAdmin(admin.ModelAdmin):
+    list_display = ["kysymys", "teksti"]
--- kysely/models.py
+++ kysely/models.py
@@ -5,8 +5,22 @@ class Kysymys(models.Model):
     kysymysteksti = models.CharField(max_length=200)
     julkaisupvm = models.DateTimeField("julkaistu")
 
+    class Meta:
+        verbose_name = "kysymys"
+        verbose_name_plural = "kysymykset"
+
+    def __str__(self):
+        return self.kysymysteksti
+
 
 class Vaihtoehto(models.Model):
     kysymys = models.ForeignKey(Kysymys, on_delete=models.CASCADE)
     teksti = models.CharField(max_length=200)
     aania = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name = "Vaihtoehto"
+        verbose_name_plural = "Vaihtoehdot"
+
+    def __str__(self):
+        return self.teksti
