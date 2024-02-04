6472c5ee246d2f9fc44d923967d90b247971b5e3
wimpula <156064473+wimpula@users.noreply.github.com>
2024-02-04 14:49:39 +0200

    lisätään näkymiä

--- kysely/urls.py
+++ kysely/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
+    # ex: /kyselyt/
     path("", views.index, name="index"),
+    # ex: /kyselyt/5/
+    path("<int:question_id>/", views.yksityiskohdat, name="yksityiskohdat"),
+    # ex: /kyselyt/5/results/
+    path("<int:question_id>/tulokset/", views.tulokset, name="tulokset"),
+    # ex: /kyselyt/5/vote/
+    path("<int:question_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
--- kysely/views.py
+++ kysely/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Moro. Oot kysely-appin index-sivul.")
 
+def yksityiskohdat(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def tulokset(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def äänestä(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
